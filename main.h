dbf40e9405799297c42afb99207def4361e3600a
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2023-07-21 20:05:52 -0700

    First Implementation

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+va_list list, char buffer[], int flags, int width, int precision, int size);
+#endif
