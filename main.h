ec840b63307ec7f477c48fc228d9ef5eddcb506f
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2023-07-26 17:21:19 -0700

    Update

--- main.h
+++ main.h
@@ -16,4 +16,20 @@ int print_octal(unsigned int b);
 char *_str_rev(char *str);
 int print_hexadecimal(unsigned int b);
 int _printf_cont(char c, va_list ap);
+int is_digit(char c);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
 #endif
